8e7d26241ab42650c73dfe5880d128cf45930d35
noear <noear@live.cn>
2022-10-25 14:34:56 +0800

    init

--- rubber-admin/src/main/java/rubberadmin/dso/db/DbWaterCfgApi.java
+++ rubber-admin/src/main/java/rubberadmin/dso/db/DbWaterCfgApi.java
@@ -1,22 +1,13 @@
 package rubberadmin.dso.db;
 
-import org.noear.solon.Utils;
-import org.noear.water.WW;
-import org.noear.water.dso.NoticeUtils;
-import org.noear.water.utils.Base64Utils;
-import org.noear.water.utils.TextUtils;
 import org.noear.wood.DbContext;
-import org.noear.wood.DbTableQuery;
 import rubberadmin.Config;
 import rubberadmin.dso.CacheUtil;
 import rubberadmin.dso.ConfigType;
-import rubberadmin.models.TagCountsModel;
 import rubberadmin.models.water_cfg.ConfigModel;
 
 import java.sql.SQLException;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class DbWaterCfgApi {
 
@@ -24,168 +15,6 @@ public class DbWaterCfgApi {
         return Config.water;
     }
 
-
-
-    public static void delConfigByIds(int act, String ids) throws SQLException {
-        List<Object> list = Arrays.asList(ids.split(",")).stream().map(s -> Integer.parseInt(s)).collect(Collectors.toList());
-
-        if (list.size() == 0) {
-            return;
-        }
-
-        if (act == 9) {
-            db().table("water_cfg_properties")
-                    .whereIn("row_id", list)
-                    .delete();
-        } else {
-            db().table("water_cfg_properties")
-                    .set("is_enabled", (act == 1 ? 1 : 0))
-                    .set("gmt_modified", System.currentTimeMillis())
-                    .whereIn("row_id", list)
-                    .update();
-        }
-    }
-
-
-    //导入
-    public static void impConfig(String tag, ConfigModel wm) throws SQLException {
-        if(TextUtils.isEmpty(tag) == false){
-            wm.tag = tag;
-        }
-
-        if(TextUtils.isEmpty(wm.tag) || TextUtils.isEmpty(wm.key) || TextUtils.isEmpty(wm.value)){
-            return;
-        }
-
-        db().table("water_cfg_properties")
-                .set("tag", wm.tag)
-                .set("type", wm.type)
-                .set("key", wm.key)
-                .set("value", wm.value)
-                .set("edit_mode", wm.edit_mode)
-                .insertBy("tag,key");
-    }
-
-    public static List<ConfigModel> getConfigByIds(String ids) throws SQLException {
-        List<Object> list = Arrays.asList(ids.split(","))
-                .stream()
-                .map(s->Integer.parseInt(s))
-                .collect(Collectors.toList());
-
-        return db().table("water_cfg_properties")
-                .whereIn("row_id", list)
-                .select("*")
-                .getList(ConfigModel.class);
-    }
-
-
-    //编辑更新config。
-    public static void setConfig(long row_id, String tag, String key, Integer type, String value0, String edit_mode) throws SQLException {
-        if (value0 == null) {
-            value0 = "";
-        } else {
-            value0 = value0.trim();
-        }
-
-        String value_org = value0;
-        String value = WW.cfg_data_header + Base64Utils.encode(value0);
-
-
-        DbTableQuery db = db().table("water_cfg_properties")
-                .set("tag", tag.trim())
-                .set("key", key.trim())
-                .set("type", type)
-                .set("edit_mode", edit_mode)
-                .set("value", value)
-                .set("gmt_modified", System.currentTimeMillis());
-
-        if (row_id > 0) {
-            db.where("row_id = ?", row_id).update();
-            NoticeUtils.updateConfig(tag, key);
-        } else {
-            row_id = db.insert();
-            NoticeUtils.updateConfig(tag, key);
-        }
-
-        /** 记录历史版本 */
-        DbWaterVerApi.logVersion(db(), "water_cfg_properties", "row_id", row_id, data -> {
-            data.put("value", value_org);
-        });
-    }
-
-    public static boolean setConfigByTagName(String tag, String key, String value) throws SQLException {
-        if (value == null) {
-            return false;
-        }
-
-        value = WW.cfg_data_header + Base64Utils.encode(value.trim());
-
-        DbTableQuery tb = db().table("water_cfg_properties")
-                .set("tag", tag.trim())
-                .set("key", key.trim())
-                .set("value", value)
-                .set("gmt_modified", System.currentTimeMillis());
-
-        tb.upsertBy("tag,key");
-
-        NoticeUtils.updateConfig(tag, key);
-
-        return true;
-    }
-
-    public static void delConfig(Integer row_id) throws SQLException {
-        if (row_id == null) {
-            return;
-        }
-
-        db().table("water_cfg_properties")
-                .where("row_id=?", row_id)
-                .delete();
-    }
-
-
-    //获取标签数组。
-    public static List<TagCountsModel> getConfigTags() throws SQLException {
-        return db().table("water_cfg_properties")
-                .groupBy("tag")
-                .orderByAsc("tag")
-                .select("tag,count(*) counts")
-                .getList(TagCountsModel.class);
-    }
-
-    // 获取有特定类型配置的TAG
-    public static List<TagCountsModel> getConfigTagsByType(int type) throws SQLException {
-        return db().table("water_cfg_properties")
-                .where("type = ?", type)
-                .groupBy("tag")
-                .orderByAsc("tag")
-                .select("tag, COUNT(*) AS counts")
-                .getList(TagCountsModel.class);
-    }
-
-    //编辑功能，根据row_id获取config信息。
-    public static ConfigModel getConfig(Integer row_id) throws SQLException {
-        if(row_id == null || row_id == 0){
-            return new ConfigModel();
-        }
-
-        return db().table("water_cfg_properties")
-                .where("row_id = ?", row_id)
-                .select("*")
-                .getItem(ConfigModel.class);
-    }
-
-
-    //根据tag列出config。
-    public static ConfigModel getConfigByTagName(String tagName) throws SQLException {
-        if(TextUtils.isEmpty(tagName)){
-            return new ConfigModel();
-        }
-
-        String[] ss = tagName.split("/");
-
-        return getConfigByTagName(ss[0], ss[1]);
-    }
 
 
     public static ConfigModel getConfigByTagName(String tag, String name) throws SQLException {
@@ -202,64 +31,12 @@ public class DbWaterCfgApi {
                 .getItem(ConfigModel.class);
     }
 
-    public static List<ConfigModel> getConfigsByTag(String tag, String key, int state) throws SQLException {
-        return db().table("water_cfg_properties")
-                .whereEq("tag", tag)
-                .andEq("is_enabled", state == 1)
-                .build(tb -> {
-                    if (!TextUtils.isEmpty(key)) {
-                        tb.and("`key` like ?", "%" + key + "%");
-                    }
-                })
-                .selectList("*", ConfigModel.class);
-    }
-
-    public static List<ConfigModel> getConfigsByType(String tag, int type) throws SQLException {
-        return db().table("water_cfg_properties")
-                .where("type = ?", type)
-                .build((tb) -> {
-                    if (TextUtils.isEmpty(tag) == false) {
-                        tb.and("tag = ?", tag);
-                    }
-                })
-                .orderBy("key")
-                .selectList("*", ConfigModel.class);
-    }
-
-    public static List<ConfigModel> getConfigTagKeyByType(String tag, int type) throws SQLException {
-        return db().table("water_cfg_properties")
-                .where("type = ?", type)
-                .build((tb) -> {
-                    if (TextUtils.isEmpty(tag) == false) {
-                        tb.and("tag = ?", tag);
-                    }
-                })
-                .selectList("tag,key", ConfigModel.class);
-    }
-
     //====================================================
 
     //获取type=10的配置（结构化数据库）
     public static List<ConfigModel> getDbConfigs() throws SQLException {
         return db().table("water_cfg_properties")
                 .whereEq("type", ConfigType.db)
-                .orderBy("`tag`,`key`")
-                .select("*")
-                .getList(ConfigModel.class);
-    }
-
-    //获取type=10的配置（结构化数据库）
-    public static List<ConfigModel> getLogStoreConfigs() throws SQLException {
-        return db().table("water_cfg_properties")
-                .whereEq("type", ConfigType.water_logger)
-                .orderBy("`tag`,`key`")
-                .select("*")
-                .getList(ConfigModel.class);
-    }
-
-    public static List<ConfigModel> getMsgStoreConfigs() throws SQLException {
-        return db().table("water_cfg_properties")
-                .whereEq("type", ConfigType.water_broker)
                 .orderBy("`tag`,`key`")
                 .select("*")
                 .getList(ConfigModel.class);
